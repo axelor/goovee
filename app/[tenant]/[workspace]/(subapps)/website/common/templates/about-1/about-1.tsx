fedb6e4ac85ec4bfc11398366c28a7f5f4fde341
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-08-21 14:43:30 +0530

    feat(website): add about-2 template (#98839)

--- app/[tenant]/[workspace]/(subapps)/website/common/templates/about-1/about-1.tsx
+++ app/[tenant]/[workspace]/(subapps)/website/common/templates/about-1/about-1.tsx
@@ -37,8 +37,8 @@ export function About1(props: TemplateProps<About1Data>) {
           </div>
 
           <div className="col-lg-5">
-            <h2 className="fs-16 text-uppercase text-muted mb-3">{title}</h2>
-            <h3 className="display-4 mb-7">{caption}</h3>
+            <h2 className="fs-16 text-uppercase text-muted mb-3">{caption}</h2>
+            <h3 className="display-4 mb-7">{title}</h3>
 
             <AccordionList accordions={accordions} />
           </div>
---
+++ app/[tenant]/[workspace]/(subapps)/website/common/templates/about-2/lightbox.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import useLightBox from '../../hooks/useLightBox';
+
+export function LightBox() {
+  useLightBox();
+  return null;
+}
