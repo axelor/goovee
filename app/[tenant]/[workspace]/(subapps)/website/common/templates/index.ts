48159f0789316906f85c82950ffd5c2750bedc0f
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-09-02 16:47:54 +0530

    feat(website): add faq-1 template (#98839)

--- app/[tenant]/[workspace]/(subapps)/website/common/templates/index.ts
+++ app/[tenant]/[workspace]/(subapps)/website/common/templates/index.ts
@@ -76,6 +76,7 @@ import {Facts14, facts14Schema} from './facts-14';
 import {Facts15, facts15Schema} from './facts-15';
 import {Facts16, facts16Schema} from './facts-16';
 import {Facts17, facts17Schema} from './facts-17';
+import {FAQ1, faq1Schema} from './faq-1';
 import {Footer1, footer1Schema} from './footer-1';
 import {Hero1, hero1Schema} from './hero-1';
 import {Navbar1, navbar1Schema} from './navbar-1';
@@ -149,6 +150,7 @@ const componentMap: Record<string, ComponentType<TemplateProps>> = {
   [facts15Schema.code]: Facts15,
   [facts16Schema.code]: Facts16,
   [facts17Schema.code]: Facts17,
+  [faq1Schema.code]: FAQ1,
   [process1Schema.code]: Process1,
   [team1Schema.code]: Team1,
   [services2Schema.code]: Services2,
