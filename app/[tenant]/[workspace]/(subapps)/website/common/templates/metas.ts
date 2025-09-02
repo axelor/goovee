8e3c78bbad9ef88036eeca04612e08ef19a6e9e8
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-09-02 16:51:56 +0530

    feat(website): add faq-4 template (#98839)

--- app/[tenant]/[workspace]/(subapps)/website/common/templates/metas.ts
+++ app/[tenant]/[workspace]/(subapps)/website/common/templates/metas.ts
@@ -74,6 +74,7 @@ import {facts17Demos, facts17Schema} from './facts-17/meta';
 import {faq1Demos, faq1Schema} from './faq-1/meta';
 import {faq2Demos, faq2Schema} from './faq-2/meta';
 import {faq3Demos, faq3Schema} from './faq-3/meta';
+import {faq4Demos, faq4Schema} from './faq-4/meta';
 import {footer1Demos, footer1Schema} from './footer-1/meta';
 import {hero1Demos, hero1Schema} from './hero-1/meta';
 import {navbar1Demos, navbar1Schema} from './navbar-1/meta';
@@ -149,6 +150,7 @@ export const metas = [
   {schema: faq1Schema, demos: faq1Demos},
   {schema: faq2Schema, demos: faq2Demos},
   {schema: faq3Schema, demos: faq3Demos},
+  {schema: faq4Schema, demos: faq4Demos},
   {schema: process1Schema, demos: process1Demos},
   {schema: team1Schema, demos: team1Demos},
   {schema: services2Schema, demos: services2Demos},
