8b0a4b11182d70f2d1e33e1e1aabba1c2fa745ef
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-09-02 17:40:04 +0530

    feat(website): add hero-9 template (#98839)

--- app/[tenant]/[workspace]/(subapps)/website/common/templates/metas.ts
+++ app/[tenant]/[workspace]/(subapps)/website/common/templates/metas.ts
@@ -100,6 +100,7 @@ import {hero5Demos, hero5Schema} from './hero-5/meta';
 import {hero6Demos, hero6Schema} from './hero-6/meta';
 import {hero7Demos, hero7Schema} from './hero-7/meta';
 import {hero8Demos, hero8Schema} from './hero-8/meta';
+import {hero9Demos, hero9Schema} from './hero-9/meta';
 import {navbar1Demos, navbar1Schema} from './navbar-1/meta';
 import {pageProgress1Demos, pageProgress1Schema} from './page-progress-1/meta';
 import {pricing1Demos, pricing1Schema} from './pricing-1/meta';
@@ -150,6 +151,7 @@ export const metas = [
   {schema: hero6Schema, demos: hero6Demos},
   {schema: hero7Schema, demos: hero7Demos},
   {schema: hero8Schema, demos: hero8Demos},
+  {schema: hero9Schema, demos: hero9Demos},
   {schema: services1Schema, demos: services1Demos},
   {schema: cta1Schema, demos: cta1Demos},
   {schema: cta2Schema, demos: cta2Demos},
