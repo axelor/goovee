78d9e5c52d130acf9f4b70298e8808613657fcdd
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-09-02 16:19:44 +0530

    feat(website): add contact-11 template (#98839)

--- app/[tenant]/[workspace]/(subapps)/website/common/templates/metas.ts
+++ app/[tenant]/[workspace]/(subapps)/website/common/templates/metas.ts
@@ -43,6 +43,7 @@ import {contact7Demos, contact7Schema} from './contact-7/meta';
 import {contact8Demos, contact8Schema} from './contact-8/meta';
 import {contact9Demos, contact9Schema} from './contact-9/meta';
 import {contact10Demos, contact10Schema} from './contact-10/meta';
+import {contact11Demos, contact11Schema} from './contact-11/meta';
 import {cta1Demos, cta1Schema} from './cta-1/meta';
 import {footer1Demos, footer1Schema} from './footer-1/meta';
 import {hero1Demos, hero1Schema} from './hero-1/meta';
@@ -106,6 +107,7 @@ export const metas = [
   {schema: contact8Schema, demos: contact8Demos},
   {schema: contact9Schema, demos: contact9Demos},
   {schema: contact10Schema, demos: contact10Demos},
+  {schema: contact11Schema, demos: contact11Demos},
   {schema: clientlist1Schema, demos: clientlist1Demos},
   {schema: clientlist2Schema, demos: clientlist2Demos},
   {schema: clientlist3Schema, demos: clientlist3Demos},
