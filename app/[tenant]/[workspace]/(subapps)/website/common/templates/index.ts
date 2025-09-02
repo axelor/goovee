7eea0ddff861c994b9596167d3ffb9c1ba5d9f6b
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-09-02 16:22:52 +0530

    feat(website): add cta-4 template (#98839)

--- app/[tenant]/[workspace]/(subapps)/website/common/templates/index.ts
+++ app/[tenant]/[workspace]/(subapps)/website/common/templates/index.ts
@@ -53,6 +53,7 @@ import {Contact12, contact12Schema} from './contact-12';
 import {CTA1, cta1Schema} from './cta-1';
 import {CTA2, cta2Schema} from './cta-2';
 import {CTA3, cta3Schema} from './cta-3';
+import {CTA4, cta4Schema} from './cta-4';
 import {Footer1, footer1Schema} from './footer-1';
 import {Hero1, hero1Schema} from './hero-1';
 import {Navbar1, navbar1Schema} from './navbar-1';
@@ -103,6 +104,7 @@ const componentMap: Record<string, ComponentType<TemplateProps>> = {
   [cta1Schema.code]: CTA1,
   [cta2Schema.code]: CTA2,
   [cta3Schema.code]: CTA3,
+  [cta4Schema.code]: CTA4,
   [process1Schema.code]: Process1,
   [team1Schema.code]: Team1,
   [services2Schema.code]: Services2,
