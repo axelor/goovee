e65d0d0fd1c62fcf39f380fda9ddf46731bae694
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-09-02 17:17:53 +0530

    feat(website): add footer-11 template (#98839)

--- app/[tenant]/[workspace]/(subapps)/website/common/templates/index.ts
+++ app/[tenant]/[workspace]/(subapps)/website/common/templates/index.ts
@@ -92,6 +92,7 @@ import {Footer7, footer7Schema} from './footer-7';
 import {Footer8, footer8Schema} from './footer-8';
 import {Footer9, footer9Schema} from './footer-9';
 import {Footer10, footer10Schema} from './footer-10';
+import {Footer11, footer11Schema} from './footer-11';
 import {Hero1, hero1Schema} from './hero-1';
 import {Navbar1, navbar1Schema} from './navbar-1';
 import {PageProgress1, pageProgress1Schema} from './page-progress-1';
@@ -203,6 +204,7 @@ const componentMap: Record<string, ComponentType<TemplateProps>> = {
   [footer8Schema.code]: Footer8,
   [footer9Schema.code]: Footer9,
   [footer10Schema.code]: Footer10,
+  [footer11Schema.code]: Footer11,
   [wiki1Schema.code]: Wiki1,
   [sidebarMenu1Schema.code]: SidebarMenu1,
 };
