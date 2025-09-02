2e9fbe0d988b5e7946ea4281efd11e0bb93f0fb8
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-09-02 16:33:18 +0530

    feat(website): add facts-6 template (#98839)

--- app/[tenant]/[workspace]/(subapps)/website/common/templates/metas.ts
+++ app/[tenant]/[workspace]/(subapps)/website/common/templates/metas.ts
@@ -59,6 +59,7 @@ import {facts2Demos, facts2Schema} from './facts-2/meta';
 import {facts3Demos, facts3Schema} from './facts-3/meta';
 import {facts4Demos, facts4Schema} from './facts-4/meta';
 import {facts5Demos, facts5Schema} from './facts-5/meta';
+import {facts6Demos, facts6Schema} from './facts-6/meta';
 import {footer1Demos, footer1Schema} from './footer-1/meta';
 import {hero1Demos, hero1Schema} from './hero-1/meta';
 import {navbar1Demos, navbar1Schema} from './navbar-1/meta';
@@ -119,6 +120,7 @@ export const metas = [
   {schema: facts3Schema, demos: facts3Demos},
   {schema: facts4Schema, demos: facts4Demos},
   {schema: facts5Schema, demos: facts5Demos},
+  {schema: facts6Schema, demos: facts6Demos},
   {schema: process1Schema, demos: process1Demos},
   {schema: team1Schema, demos: team1Demos},
   {schema: services2Schema, demos: services2Demos},
