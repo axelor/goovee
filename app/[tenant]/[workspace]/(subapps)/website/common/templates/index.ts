f24b8e7b8c4feffdd1f275fc85395dd26621c0a3
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-09-03 16:53:18 +0530

    feat(website): add portfolio-9 template (#98839)

--- app/[tenant]/[workspace]/(subapps)/website/common/templates/index.ts
+++ app/[tenant]/[workspace]/(subapps)/website/common/templates/index.ts
@@ -132,6 +132,7 @@ import {Portfolio5, portfolio5Schema} from './portfolio-5';
 import {Portfolio6, portfolio6Schema} from './portfolio-6';
 import {Portfolio7, portfolio7Schema} from './portfolio-7';
 import {Portfolio8, portfolio8Schema} from './portfolio-8';
+import {Portfolio9, portfolio9Schema} from './portfolio-9';
 import {Process1, process1Schema} from './process-1';
 import {Services1, services1Schema} from './services-1';
 import {Services2, services2Schema} from './services-2';
@@ -242,6 +243,7 @@ const componentMap: Record<string, ComponentType<TemplateProps>> = {
   [portfolio6Schema.code]: Portfolio6,
   [portfolio7Schema.code]: Portfolio7,
   [portfolio8Schema.code]: Portfolio8,
+  [portfolio9Schema.code]: Portfolio9,
   [contact1Schema.code]: Contact1,
   [contact2Schema.code]: Contact2,
   [contact3Schema.code]: Contact3,
