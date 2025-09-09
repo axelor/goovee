418c6911c45b7ca39ca247281815ef91ab42d057
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-09-09 20:27:22 +0530

    feat(website): add testimonial-14 template (#98839)

--- app/[tenant]/[workspace]/(subapps)/website/common/templates/metas.ts
+++ app/[tenant]/[workspace]/(subapps)/website/common/templates/metas.ts
@@ -199,6 +199,7 @@ import {testimonial10Demos, testimonial10Schema} from './testimonial-10/meta';
 import {testimonial11Demos, testimonial11Schema} from './testimonial-11/meta';
 import {testimonial12Demos, testimonial12Schema} from './testimonial-12/meta';
 import {testimonial13Demos, testimonial13Schema} from './testimonial-13/meta';
+import {testimonial14Demos, testimonial14Schema} from './testimonial-14/meta';
 import {wiki1Demos, wiki1Schema} from './wiki-1/meta';
 
 export const metas = [
@@ -349,6 +350,7 @@ export const metas = [
   {schema: testimonial11Schema, demos: testimonial11Demos},
   {schema: testimonial12Schema, demos: testimonial12Demos},
   {schema: testimonial13Schema, demos: testimonial13Demos},
+  {schema: testimonial14Schema, demos: testimonial14Demos},
   {schema: pricing1Schema, demos: pricing1Demos},
   {schema: pricing2Schema, demos: pricing2Demos},
   {schema: pricing3Schema, demos: pricing3Demos},
