e00165e643a7fb97a0813ce72bcf3e427ab0da91
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-09-09 16:11:56 +0530

    feat(website): add service-25 template (#98839)

--- app/[tenant]/[workspace]/(subapps)/website/common/templates/index.ts
+++ app/[tenant]/[workspace]/(subapps)/website/common/templates/index.ts
@@ -180,6 +180,7 @@ import {Service21, service21Schema} from './service-21';
 import {Service22, service22Schema} from './service-22';
 import {Service23, service23Schema} from './service-23';
 import {Service24, service24Schema} from './service-24';
+import {Service25, service25Schema} from './service-25';
 import {SidebarMenu1, sidebarMenu1Schema} from './sidebar-menu-1';
 import {Team1, team1Schema} from './team-1';
 import {Testimonial1, testimonial1Schema} from './testimonial-1';
@@ -312,6 +313,7 @@ const componentMap: Record<string, ComponentType<TemplateProps>> = {
   [service22Schema.code]: Service22,
   [service23Schema.code]: Service23,
   [service24Schema.code]: Service24,
+  [service25Schema.code]: Service25,
   [testimonial1Schema.code]: Testimonial1,
   [pricing1Schema.code]: Pricing1,
   [pricing2Schema.code]: Pricing2,
--- app/[tenant]/[workspace]/(subapps)/website/common/templates/metas.ts
+++ app/[tenant]/[workspace]/(subapps)/website/common/templates/metas.ts
@@ -175,6 +175,7 @@ import {service21Demos, service21Schema} from './service-21/meta';
 import {service22Demos, service22Schema} from './service-22/meta';
 import {service23Demos, service23Schema} from './service-23/meta';
 import {service24Demos, service24Schema} from './service-24/meta';
+import {service25Demos, service25Schema} from './service-25/meta';
 import {sidebarMenu1Demos, sidebarMenu1Schema} from './sidebar-menu-1/meta';
 import {team1Demos, team1Schema} from './team-1/meta';
 import {testimonial1Demos, testimonial1Schema} from './testimonial-1/meta';
@@ -305,6 +306,7 @@ export const metas = [
   {schema: service22Schema, demos: service22Demos},
   {schema: service23Schema, demos: service23Demos},
   {schema: service24Schema, demos: service24Demos},
+  {schema: service25Schema, demos: service25Demos},
   {schema: team1Schema, demos: team1Demos},
   {schema: testimonial1Schema, demos: testimonial1Demos},
   {schema: pricing1Schema, demos: pricing1Demos},
