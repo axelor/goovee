abfde25f8ae4c9973139d3ee4d062b4039ddd987
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-09-09 13:40:41 +0530

    feat(website): add service-11 template (#98839)

--- app/[tenant]/[workspace]/(subapps)/website/common/templates/index.ts
+++ app/[tenant]/[workspace]/(subapps)/website/common/templates/index.ts
@@ -166,6 +166,7 @@ import {Service7, service7Schema} from './service-7';
 import {Service8, service8Schema} from './service-8';
 import {Service9, service9Schema} from './service-9';
 import {Service10, service10Schema} from './service-10';
+import {Service11, service11Schema} from './service-11';
 import {SidebarMenu1, sidebarMenu1Schema} from './sidebar-menu-1';
 import {Team1, team1Schema} from './team-1';
 import {Testimonial1, testimonial1Schema} from './testimonial-1';
@@ -284,6 +285,7 @@ const componentMap: Record<string, ComponentType<TemplateProps>> = {
   [service8Schema.code]: Service8,
   [service9Schema.code]: Service9,
   [service10Schema.code]: Service10,
+  [service11Schema.code]: Service11,
   [testimonial1Schema.code]: Testimonial1,
   [pricing1Schema.code]: Pricing1,
   [pricing2Schema.code]: Pricing2,
