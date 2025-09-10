be2e6efd79fe7cadcf0e90abc9b8d567cf3ae36a
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-09-10 11:58:45 +0530

    feat(website): add banner-2 template (#98839)

--- app/[tenant]/[workspace]/(subapps)/website/common/templates/index.ts
+++ app/[tenant]/[workspace]/(subapps)/website/common/templates/index.ts
@@ -34,6 +34,7 @@ import {Blog3, blog3Schema} from './blog-3';
 import {Blog4, blog4Schema} from './blog-4';
 import {Blog5, blog5Schema} from './blog-5';
 import {Banner1, banner1Schema} from './banner-1';
+import {Banner2, banner2Schema} from './banner-2';
 import {Clientlist1, clientlist1Schema} from './clientlist-1';
 import {Clientlist2, clientlist2Schema} from './clientlist-2';
 import {Clientlist3, clientlist3Schema} from './clientlist-3';
@@ -245,6 +246,7 @@ const componentMap: Record<string, ComponentType<TemplateProps>> = {
   [blog4Schema.code]: Blog4,
   [blog5Schema.code]: Blog5,
   [banner1Schema.code]: Banner1,
+  [banner2Schema.code]: Banner2,
   [hero1Schema.code]: Hero1,
   [hero2Schema.code]: Hero2,
   [hero3Schema.code]: Hero3,
