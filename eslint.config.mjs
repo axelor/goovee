c934896e70f294eede3a607831a0911c352d3fc2
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-12-26 16:09:22 +0530

    chore(core): set up tsconfig eslint prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,21 @@
-import { defineConfig } from "eslint/config";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import {defineConfig, globalIgnores} from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier/flat';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettier,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+]);
 
-export default defineConfig([{
-    extends: [...nextCoreWebVitals],
-}]);
+export default eslintConfig;
+
