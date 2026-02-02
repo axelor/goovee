a372d0b499f9e137fdaeba12fc750ed2c9aa9681
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2026-02-02 18:35:07 +0530

    feat(core): add google and keycloak auth config based on environment

--- lib/core/auth/(ee)/google.ts
+++ lib/core/auth/(ee)/google.ts
@@ -1,6 +1,11 @@
+import type {GoogleOptions} from 'better-auth/types';
+
 const google = {
+  enabled: process.env.SHOW_GOOGLE_OAUTH === 'true',
   clientId: process.env.GOOGLE_CLIENT_ID as string,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+} satisfies GoogleOptions & {
+  enabled?: boolean | undefined;
 };
 
 export default google;
