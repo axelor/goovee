d16441752b8516ab0b5f240b93ce4ebaebfd2164
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2026-04-01 18:16:10 +0530

    fix(core): add headers for security

--- next.config.mjs
+++ next.config.mjs
@@ -28,6 +28,42 @@ const nextConfig = {
     },
   },
   basePath: process.env.GOOVEE_PUBLIC_BASE_PATH,
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff',
+        },
+        {
+          key: 'X-Frame-Options',
+          value: 'DENY',
+        },
+        {
+          key: 'Referrer-Policy',
+          value: 'strict-origin-when-cross-origin',
+        },
+      ],
+    },
+    {
+      source: '/sw.js',
+      headers: [
+        {
+          key: 'Content-Type',
+          value: 'application/javascript; charset=utf-8',
+        },
+        {
+          key: 'Cache-Control',
+          value: 'no-cache, no-store, must-revalidate',
+        },
+        {
+          key: 'Content-Security-Policy',
+          value: "default-src 'self'; script-src 'self'",
+        },
+      ],
+    },
+  ],
 };
 
 const bundleAnalyzer = withBundleAnalyzer({
