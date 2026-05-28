b4980916d42a30472e87e2ead11fc0ff5819c084
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2026-05-28 17:01:50 +0530

    feat(marketplace): lighten gradient 6 (#111822)

--- app/[tenant]/[workspace]/(subapps)/marketplace/common/constants/gradients.ts
+++ app/[tenant]/[workspace]/(subapps)/marketplace/common/constants/gradients.ts
@@ -4,7 +4,7 @@ export const GRADIENT_MAP: Record<string, string> = {
   'gradient-3': 'from-[#d0eedd] to-[#4fc179]',
   'gradient-4': 'from-[#d0e7ff] to-[#2d60c4]',
   'gradient-5': 'from-[#fbd0e1] to-[#f26da0]',
-  'gradient-6': 'from-[#1c1f55] to-[#5603ad]',
+  'gradient-6': 'from-[#e5e0ff] to-[#a78bfa]',
   'gradient-7': 'from-[#fbc6c4] to-[#f14e46]',
   'gradient-8': 'from-[#eaf8fb] to-[#89cff0]',
   'gradient-9': 'from-[#e3daf1] to-[#643b9f]',
