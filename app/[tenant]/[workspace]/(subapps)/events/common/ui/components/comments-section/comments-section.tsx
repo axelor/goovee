9fdd7666c8fea959635cf893335432566da07f2b
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2026-07-22 13:23:26 +0530

    fix(events): remove duplicate comments label (#111812)

--- app/[tenant]/[workspace]/(subapps)/events/common/ui/components/comments-section/comments-section.tsx
+++ app/[tenant]/[workspace]/(subapps)/events/common/ui/components/comments-section/comments-section.tsx
@@ -2,7 +2,6 @@
 
 // ---- CORE IMPORTS ---- //
 import {Card} from '@/ui/components';
-import {i18n} from '@/locale';
 import {SORT_TYPE, Comments} from '@/comments';
 import {SUBAPP_CODES} from '@/constants';
 import {useWorkspace} from '@/app/[tenant]/[workspace]/workspace-context';
@@ -19,8 +18,6 @@ export const CommentsSection = ({eventId, slug}: CommentSectionProps) => {
   const {workspaceURI} = useWorkspace();
   return (
     <Card className="rounded-2xl border-none shadow-none p-4 w-full space-y-4 ">
-      <h2 className="text-xl font-semibold">{i18n.t('Comments')}</h2>
-
       <Comments
         variant="conversation"
         recordId={eventId}
